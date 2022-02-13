1f35832f6d87e17f5942a38a5991cbeb9939687b
nickadamson <naburner@protonmail.com>
2022-02-12 17:52:50 -0800

    pass vars to constructor

--- packages/hardhat/contracts/GrantFactory.sol
+++ packages/hardhat/contracts/GrantFactory.sol
@@ -13,17 +13,26 @@ contract GrantFactory {
     }
 
     function createGrant(
+        address _creator,
+        string memory _title,
+        string memory _description,
+        string memory _category,
+        string memory _genre,
+        uint8 _timeline,
         uint256 _minFundingAmount,
-        string _creator,
-        string _title,
-        string _description,
-        string _category,
-        string _genre,
-        string _deliverableFormat,
-        string _timeline
+        string memory _deliverableFormat
     ) external returns (address grantProxy) {
         grantProxy = address(
-            new GrantProxy{salt: keccak256(abi.encode(_creator, _title))}()
+            new GrantProxy{salt: keccak256(abi.encode(_creator, _title))}(
+                _creator,
+                _title,
+                _description,
+                _category,
+                _genre,
+                _timeline,
+                _minFundingAmount,
+                _deliverableFormat
+            )
         );
 
         emit GrantCreated(grantProxy, _creator);
