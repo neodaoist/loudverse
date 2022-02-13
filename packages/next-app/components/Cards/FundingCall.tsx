d3cb57346aea5e5475d0e12bce232eec4e431959
John <john@pop-os.localdomain>
2022-02-13 11:37:09 -0700

    Start layout for funding call cards

--- packages/next-app/components/Cards/FundingCall.tsx
+++ packages/next-app/components/Cards/FundingCall.tsx
@@ -1,14 +1,22 @@
-import { Box } from "degen";
 import React from "react";
+import { Box, Heading, Text } from "degen";
+import styles from "./FundingCall.module.css";
+
 
 const FundingCall = ({ callForFunding }: { callForFunding: CallForFundsGraphType }) => {
   return (
-    <Box>FundingCall</Box>
-    //name
-    //description
-    //category
-    //image
-    //stats
+    <Box backgroundColor="backgroundSecondary" padding="4" borderRadius="medium" maxWidth="168">
+      <div className={styles.titleRow}>
+        <Heading>{callForFunding.title}</Heading>
+        <Text>{callForFunding.category}</Text>
+      </div>
+      <Text>{callForFunding.description}</Text>
+      <div>[Image]</div>
+      <Text>$X funded by Y people so far</Text>
+    </Box>
+    // Still need:
+    // image
+    // stats
   );
 };
 
