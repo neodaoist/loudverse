0bf062d14ca35012637517f75fa3f9b228ae6b27
nickadamson <naburner@protonmail.com>
2022-02-12 17:52:39 -0800

    comment out for compiler

--- packages/hardhat/contracts/CryptoCredential.sol
+++ packages/hardhat/contracts/CryptoCredential.sol
@@ -7,33 +7,45 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 // plan to redo this as ERC1238 — https://github.com/violetprotocol/ERC1238-token
 
-contract CryptoCredential is ERC1155, Ownable, ERC1155Supply, NonTransferrable {
+//TODO
+// soulbound/nontransferrable
+// overwrite ERC1155's transfer func
+
+contract CryptoCredential is ERC1155, Ownable, ERC1155Supply {
     constructor() ERC1155("") {}
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
-    {
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
         _mint(account, id, amount, data);
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
         _mintBatch(to, ids, amounts, data);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        override(ERC1155, ERC1155Supply)
-    {
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155, ERC1155Supply) {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
