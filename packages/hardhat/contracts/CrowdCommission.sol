7e3f2ac2a1ab62d9fdb3173d7a5126ef05021973
neodaoist <tomMobileDev123@me.me>
2022-02-13 21:00:54 -0700

    Add basic CrowdCommission, probably with many properties that should be metadata

--- packages/hardhat/contracts/CrowdCommission.sol
+++ packages/hardhat/contracts/CrowdCommission.sol
@@ -2,34 +2,59 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CrowdCommission is ERC721, AccessControl {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                        //
+//     __        ______    __    __   _______  ____    ____  _______  .______        ______.  _______     //
+//    |  |      /  __  \  |  |  |  | |       \ \   \  /   / |_______| |   _  \      /      | |_______|    //
+//    |  |     |  |  |  | |  |  |  | |  .--.  | \   \/   /   _______  |  |_)  |    |   (---`  _______     //
+//    |  |     |  |  |  | |  |  |  | |  |  |  |  \      /   |_______| |      /      \   \    |_______|    //
+//    |  `----.|  `--'  | |  `--'  | |  '--'  |   \    /     _______  |  |\  \---.---)   |    _______     //
+//    |_______| \______/   \______/  |_______/     \__/     |_______| | _| `.____|______/    |_______|    //
+//                                                                                                        //
+//                                                                                                        //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+contract CrowdCommission is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("CrowdCommission", "loudCOMM") {
+    // Creative TODO Should most of this just be metadata ?
+    address public creator;
+    string public title;
+    string public description;
+    string public category; // (Music, Photography, Painting, Digital Art, Animation, Film, Sculpture, Poetry, Theater, Dance)
+    string public genre;
+    string public subgenre;
+    string public deliverableMedium;
+    uint8 public timelineInDays;
+
+    // TODO Add Calls for Collaborators
+
+    // Financial
+    string public fundingRound;
+    mapping(address => uint256) public crowdCommissioners;
+    uint256 public matchingFunds;
+    uint256 public totalFunds;
+
+    constructor() ERC721("CrowdCommission", "LOUD") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function safeMint(address to) public onlyRole(MINTER_ROLE) {
+    function mintCrowdCommission(
+        // args
+    ) public onlyOwner {
+
+    }
+
+    function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, AccessControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
