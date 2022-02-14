b491499b199e4e6ccbbdbdaede2676275090cc11
John <john@pop-os.localdomain>
2022-02-13 20:36:53 -0700

    Set up call details page

--- packages/next-app/components/FullPageProject.tsx
+++ packages/next-app/components/FullPageProject.tsx
@@ -1,14 +1,52 @@
-const FullPageCallDetails = () => {
-  return (
-    <div>FullPageCallDetails</div>
-    //left col
-    //visual content
-    //details
+import { Box, Stack, Text } from "degen";
+import CallDetails from "./Cards/CallDetails";
+import FundingProgress from "./Cards/FundingProgress";
+import ContributionList from "./Cards/ContributionList";
 
-    //right col
-    //funding progress goal
-    //if funded, show owner button to stream
-    //tx log
+const FullPageCallDetails = () => {
+  // Dummy data
+  const call = {
+    logicAddress: "0x1",
+    minFundingAmount: "100 DAI",
+    creator: "person1",
+    title: "Solarpunk Strings",
+    description: "@wellwisher.eth is creating a musical solarpunk experience with a Western classical twist",
+    category: "music",
+    genre: "Classical Music",
+    subgenre: "String Quartet",
+    deliverableFormat: "Recording (mp3), Score (pdf)",
+    timeline: "90 days",
+    fundingState: "open",
+  };
+  const txLog = [
+    {
+      funder: "@funder1.eth",
+      amount: "100",
+      timestamp: "Sun Feb 13 2022 20:13:28",
+    },
+    {
+      funder: "@funder2.eth",
+      amount: "20",
+      timestamp: "Sun Feb 13 2022 20:13:28",
+    },
+    {
+      funder: "@funder3.eth",
+      amount: "35",
+      timestamp: "Sun Feb 13 2022 20:13:28",
+    },
+  ];
+
+  return (
+    <Box>
+      <Box>First row (connect button, anything else)</Box>
+      <Stack direction="horizontal">
+        <CallDetails callForFunding={call} />
+        <Stack>
+          <FundingProgress callForFunding={call} />
+          <ContributionList contributionList={txLog} />
+        </Stack>
+      </Stack>
+    </Box>
   );
 };
 
