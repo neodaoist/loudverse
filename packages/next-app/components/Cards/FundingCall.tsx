020bd141b37e454eff32f51421a861f858efbfcd
John <john@pop-os.localdomain>
2022-02-13 17:00:01 -0700

    Set up calls grid, use degen for everything

--- packages/next-app/components/Cards/FundingCall.tsx
+++ packages/next-app/components/Cards/FundingCall.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { Box, Heading, Text } from "degen";
-import styles from "./FundingCall.module.css";
-
+import { Box, Tag, Text } from "degen";
 
 const FundingCall = ({ callForFunding }: { callForFunding: CallForFundsGraphType }) => {
   return (
-    <Box backgroundColor="backgroundSecondary" padding="4" borderRadius="medium" maxWidth="168">
-      <div className={styles.titleRow}>
-        <Heading>{callForFunding.title}</Heading>
-        <Text>{callForFunding.category}</Text>
-      </div>
+    <Box backgroundColor="foregroundSecondary" padding="4" borderRadius="medium" width="112" height="64">
+      <Box display="flex" justifyContent="space-between" alignItems="baseline">
+        <Text size="extraLarge">{callForFunding.title}</Text>
+        <Tag tone="accent">{callForFunding.category}</Tag>
+      </Box>
       <Text>{callForFunding.description}</Text>
-      <div>[Image]</div>
+      <Box>[Image]</Box>
       <Text>$X funded by Y people so far</Text>
     </Box>
     // Still need:
