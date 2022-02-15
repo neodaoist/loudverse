ecac1e2c5fa025e53713693f2f0f0348d6d67158
nickadamson <naburner@protonmail.com>
2022-02-14 19:33:21 -0800

    add page wrapper add data-fetching

--- packages/next-app/pages/calls/[id].tsx
+++ packages/next-app/pages/calls/[id].tsx
@@ -1,13 +1,41 @@
-import type { NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css"; // change
-import { useRouter } from "next/router";
 import FullPageCallDetails from "../../components/FullPageProject";
+import { getCallForFundsByID, getAllCallsForFunds } from "../../graph/functions";
+import { CallForFunding } from "../../graph/loudverse-graph-types";
+import PageWrapper from "../../components/Layout/PageWrapper";
 
-const Call: NextPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return <FullPageCallDetails proxyAddress={id} />;
+const Call = ({ call }: { call: CallForFunding }) => {
+  return (
+    <PageWrapper>
+      <FullPageCallDetails call={call} />
+    </PageWrapper>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allCalls = await getAllCallsForFunds();
+  const paths = [];
+
+  allCalls.forEach((call, i) => {
+    paths.push({ params: { id: `${i}` } });
+  });
+
+  return { paths, fallback: true };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { id } = context.params;
+  const allCalls = await getAllCallsForFunds();
+
+  const call = allCalls[Number(id)];
+
+  return {
+    props: {
+      call: call,
+    },
+  };
 };
 
 export default Call;
--- packages/next-app/pages/calls/create.tsx
+++ packages/next-app/pages/calls/create.tsx
@@ -2,9 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 import NewProjectForm from "../../components/Forms/NewProjectForm"; // change
+import PageWrapper from "../../components/Layout/PageWrapper";
 
 const Create: NextPage = () => {
-  return <NewProjectForm />;
+  return (
+    <PageWrapper>
+      <NewProjectForm />
+    </PageWrapper>
+  );
 };
 
 export default Create;
