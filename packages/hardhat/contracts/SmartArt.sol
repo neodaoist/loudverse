609ea71c562e42fb7730d602c8533cd9daf8f4fd
neodaoist <tomMobileDev123@me.me>
2022-02-16 11:21:44 -0700

    Add initial ERC2981 Royalty support

--- packages/hardhat/contracts/SmartArt.sol
+++ packages/hardhat/contracts/SmartArt.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import './ERC2981/ERC2981PerTokenRoyalties.sol';
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                                        //
@@ -18,7 +19,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 //                                                                                                        //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-contract SmartArt is ERC721, ERC721Burnable, AccessControl {
+contract SmartArt is ERC721, ERC721Burnable, AccessControl, ERC2981PerTokenRoyalties {
     using Counters for Counters.Counter;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -28,6 +29,45 @@ contract SmartArt is ERC721, ERC721Burnable, AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
+
+    /**
+    These 2 methods are from https://github.com/dievardump/EIP2981-implementation
+     */
+
+    /// @inheritdoc	ERC165
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, ERC2981Base)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    /// @notice Mint one token to `to`
+    /// @param to the recipient of the token
+    /// @param royaltyRecipient the recipient for royalties (if royaltyValue > 0)
+    /// @param royaltyValue the royalties asked for (EIP2981)
+    function mint(
+        address to, // this will be the Creator address
+        address royaltyRecipient, // this will be the Slicer address
+        uint256 royaltyValue // this will be 10% so 1000
+    ) external {
+        uint256 tokenId = nextTokenId;
+        _safeMint(to, tokenId, '');
+
+        if (royaltyValue > 0) {
+            _setTokenRoyalty(tokenId, royaltyRecipient, royaltyValue);
+        }
+
+        nextTokenId = tokenId + 1;
+    }
+
+
+    /**
+    These 2 methods are from OpenZeppelin Contract Wizard
+     */
 
     function safeMint(address to) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
