4ebd6bf1807234912e385f0afdd0ad4ab23cb735
nickadamson <naburner@protonmail.com>
2022-02-18 12:27:28 -0700

    add nft.storage boilerplate

--- packages/next-app/utils/index.ts
+++ packages/next-app/utils/index.ts
@@ -1,3 +1,23 @@
+import { NFTStorage, File } from "nft.storage";
+
+const apiKey = process.env.NFT_STORAGE_KEY;
+const client = new NFTStorage({ token: apiKey });
+const uploadNFT = async (): Promise<string> => {
+  const metadata = await client.store({
+    name: "Pinpie",
+    description: "Pin is not delicious beef!",
+    image: new File(
+      [
+        /* data */
+      ],
+      "pinpie.jpg",
+      { type: "image/jpg" },
+    ),
+  });
+
+  return metadata.url;
+};
+
 export function toTrimmedAddress(value: string): string {
   if (!value) return "";
   return `${value.substr(0, 6)}…${value.substr(value.length - 4, value.length)}`;
