353a29655772f9c4fe77f59bb2d653d6448274d2
John <john@pop-os.localdomain>
2022-02-19 12:20:24 -0700

    User details page updates

--- packages/next-app/components/Cards/UserStats.tsx
+++ packages/next-app/components/Cards/UserStats.tsx
@@ -6,11 +6,11 @@ const UserStats = ({ stats }) => {
     <Box backgroundColor="foregroundSecondary" borderRadius="medium" padding="4" borderWidth="0.5" borderColor="accent">
       <Box marginBottom="4">
         <Text size="large">
-          Raised {stats.raised} DAI for {stats.projects} projects
+          Raised {stats.raised} ETH for {stats.projects} projects
         </Text>
       </Box>
       <Box marginBottom="4">
-        <Text size="large">Collaboarated with {stats.collaborators} creators</Text>
+        <Text size="large">Collaboarated with {stats.collaborators} other creators</Text>
       </Box>
       <Box>
         <Text size="large">Currently working on {stats.current}</Text>
