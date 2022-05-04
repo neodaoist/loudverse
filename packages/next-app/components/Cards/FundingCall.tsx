c7621551d846760ddcf9611dd01d85a177cf1b3d
nickadamson <naburner@protonmail.com>
2022-05-04 14:01:28 -0700

    hardcode photo for call 4

--- packages/next-app/components/Cards/FundingCall.tsx
+++ packages/next-app/components/Cards/FundingCall.tsx
@@ -69,7 +69,11 @@ const FundingCall = ({ callForFunding, index }: { callForFunding: CallForFunding
           alignItems="center"
         >
           <Image
-            src={callForFunding?.image === " " ? call6 : callForFunding?.image}
+            src={
+              callForFunding.id === "0xc2db902e79144c9d572f148ac14d20045e420356"
+                ? "https://infura-ipfs.io/ipfs/bafybeieaczhi3egn2ydcmgt2366ifveacp73mirsyt6icevk7u6wx25z7i"
+                : callForFunding?.image
+            }
             alt="Call For Funding's cover image"
             layout="fill"
             objectFit="cover"
