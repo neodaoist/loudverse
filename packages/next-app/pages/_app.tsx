81747fd3f2424bbb2a26b6293135a8dbd988600f
nickadamson <naburner@protonmail.com>
2022-05-06 11:02:05 -0700

    conditionally (prod only) inject gtag

--- packages/next-app/pages/_app.tsx
+++ packages/next-app/pages/_app.tsx
@@ -12,13 +12,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
+    if (process.env.NODE_ENV === "production") {
+      const handleRouteChange = (url: URL) => {
+        gtag.pageview(url);
+      };
+      router.events.on("routeChangeComplete", handleRouteChange);
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }
   }, [router.events]);
 
   return (
