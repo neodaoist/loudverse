903521e78067adced121842d115a138fa55c5953
nickadamson <naburner@protonmail.com>
2022-05-06 11:00:16 -0700

    create Head tag component to use in all pages

---
+++ packages/next-app/components/Layout/HTMLHead.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+const HTMLHead = () => {
+  return (
+    <Head>
+      <title>LOUDVΞRSΞ</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default HTMLHead;
--- packages/next-app/components/Layout/PageWrapper.tsx
+++ packages/next-app/components/Layout/PageWrapper.tsx
@@ -1,16 +1,20 @@
 import { Box } from "degen";
 import Footer from "./Footer";
+import HTMLHead from "./HTMLHead";
 import Navbar from "./Navbar";
 
 const PageWrapper = ({ children }) => {
   return (
-    <Box height="viewHeight" display="flex" alignItems="center" flexDirection="column">
-      <Navbar />
-      <Box width="full" maxWidth="screenXl" marginX="16" paddingBottom="16" backgroundColor="backgroundSecondary">
-        {children}
+    <>
+      <HTMLHead />
+      <Box height="viewHeight" display="flex" alignItems="center" flexDirection="column">
+        <Navbar />
+        <Box width="full" maxWidth="screenXl" marginX="16" paddingBottom="16" backgroundColor="backgroundSecondary">
+          {children}
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </>
   );
 };
 
