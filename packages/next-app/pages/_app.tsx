3ad530cef2473b842b9fec7f6b6a9eefd1f2b64f
nickadamson <naburner@protonmail.com>
2022-05-06 11:01:27 -0700

    inject gtag

--- packages/next-app/pages/_app.tsx
+++ packages/next-app/pages/_app.tsx
@@ -1,11 +1,26 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
 import { WagmiProvider } from "wagmi";
 import { client } from "../utils/wagmiConfig";
 import { ThemeProvider } from "degen";
 import "degen/styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider defaultMode={pageProps.mode || "light"}>
       <WagmiProvider client={client}>
